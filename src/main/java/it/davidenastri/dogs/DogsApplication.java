f828a9e8e81e68c8372514b53e865f7e9cb05feb
Davide Nastri <d.nastri@gmail.com>
2020-08-03 23:51:44 +0200

    feat: Added Eureka client properties

--- src/main/java/it/davidenastri/dogs/DogsApplication.java
+++ src/main/java/it/davidenastri/dogs/DogsApplication.java
@@ -2,8 +2,10 @@ package it.davidenastri.dogs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class DogsApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/it/davidenastri/dogs/repository/DogRepository.java
@@ -0,0 +1,10 @@
+package it.davidenastri.dogs.repository;
+
+import it.davidenastri.dogs.entity.Dog;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface DogRepository extends CrudRepository<Dog, Long> {
+}
